eebfda1ca81c360d0097f75cb7226b5250a49beb
dramirez-bluon <dramirez@bluon.com>
2024-02-24 19:31:39 -0500

    fixed env config

--- docker/stress-tester/index.js
+++ docker/stress-tester/index.js
@@ -3,8 +3,8 @@ const BE_ENDPOINT = require('./config').BE_ENDPOINT
 const NUM_TESTS = require('./config').NUM_TESTS
 
 for (let i = 0; i < NUM_TESTS; i++) {
-    console.log("CALLED SENT")
     axios.get(BE_ENDPOINT).then(res => {
         console.log(`${res} was returned`)
     })
+    console.log("CALLED SENT")
 }
